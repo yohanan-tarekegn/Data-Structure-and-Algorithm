5effc9f60687d5da42dba36eeac288591b57f72f
yohanan Tarekegn <johanantarekegn@gmail.com>
2026-09-09 10:10:01 +0300

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <set>           // std::set
+#include <unordered_set> // std::unordered_set   
+#include <algorithm>
+ 
+  
+ 
+using namespace std;
+ 
+int main() {
+	
+	string y;
+	cin>>y;
+	y[0]=toupper(y[0]);
+	cout<<y;
+    return 0;
+}
